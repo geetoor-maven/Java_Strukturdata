6489f858ce46439eafd03385f20bd508b0008e0a
shegi-developer <agus40994@gmail.com>
2020-07-15 05:43:54 +0800

    Say thanks

--- Array/Aplikasi.java
+++ Array/Aplikasi.java
@@ -12,7 +12,7 @@ public class Aplikasi {
         // Inialisasi semua variabel
         boolean kondisi = false;
         int data,pilihan;
-        String merek;
+        String merek,hapusmerek;
         
         Scanner input = new Scanner(System.in);
         
@@ -20,15 +20,9 @@ public class Aplikasi {
          System.out.println("_________________________________");
          System.out.print("-- Berapa merek mobil sekarang : ");
          data = input.nextInt();
+         System.out.println("_________________________________");
+         
          Mobil [] mobil = new Mobil[data];
-        
-//        System.out.println("_________________________________");
-//        System.out.println("-------- SEMUA MEREK MOBIL ---------");
-//        System.out.println("_________________________________");
-//        
-//        for(int i = 0; i<mobil.length; i++){
-//            System.out.println("-- Merek mobil ke "+(i+1)+" : "+mobil[i].getMerek());
-//        }
         
         while(kondisi == false){
             System.out.println("Pilihan Menu : ");
@@ -37,12 +31,14 @@ public class Aplikasi {
             System.out.println("3. Hapus Data Mobil  ");
             System.out.println("4. Cari Data Mobil  ");
             System.out.println("0. Keluar Program ");
-            System.out.print("Masukan Menu Pilihan[1,2,3,4,5,6] : ");
+            System.out.print("Masukan Menu Pilihan[1,2,3,4,5,0] : ");
             pilihan = input.nextInt();
             
             switch(pilihan){
                 
                 case 1:
+                           System.out.println("_________________________________");
+                           System.out.println("-------- INPUT MEREK MOBIL -----------");
                            System.out.println("_________________________________");
 
                                     for(int i = 0; i<mobil.length; i++){
@@ -53,11 +49,54 @@ public class Aplikasi {
                                     }
                            System.out.println("_________________________________");         
                            break;
-                           
+                
+                case 2:
+                            System.out.println("_________________________________");
+                            System.out.println("-------- SEMUA MEREK MOBIL ---------");
+                            System.out.println("_________________________________");
+                                
+                                    for(int i = 0; i<mobil.length; i++){
+                                            System.out.println("-- Merek mobil ke "+(i+1)+" : "+mobil[i].getMerek());
+                                    }
+                                    
+                            System.out.println("_________________________________");         
+                           break;
+                 
+                case 3:
+                            System.out.println("_________________________________");
+                            System.out.println("-------- HAPUS MEREK MOBIL ---------");
+                            System.out.println("_________________________________");
+                            System.out.print("-- Masukan merek mobil yang ingin dihapus : ");
+                            hapusmerek = input.next();
+                            
+                                    for(int i = 0; i<mobil.length; i++){
+                                            mobil[i] = new Mobil();
+                                            if(hapusmerek.equals(mobil[i].getMerek())){
+                                                    
+                                                       for(int j = i; j<mobil.length-1; j++){
+                                                            
+                                                                mobil[j] =  mobil[j+1];
+                                                       }
+                                                       break;
+                                            }
+                                        
+                                    }
+                            
+                            break;
+                            
+                case 4:
+                            System.out.println("_________________________________");
+                            System.out.println("-------- CARI MEREK MOBIL -------------");
+                            System.out.println("_________________________________");
+                            
+                            
+                            break;
+                            
                 case 0:
                             kondisi = true;
                             System.out.println("--------------- TERIMA KASIH ------------------");
                             System.out.println("------- develop by : agus kurniawan --------");
+                            break;
                 default:
                             System.out.println("-----------MAAF INPUTAN TIDAK TERSEDIA----------");
             
